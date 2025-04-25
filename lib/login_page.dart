16b197542094a4ce73b1d8de2b47a02116751227
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-04-25 14:00:03 +0700

    membuat login login page

---
+++ lib/login_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+  
+  class LoginPage extends StatefulWidget {
+    const LoginPage({super.key});
+  
+    @override
+    State<LoginPage> createState() => _LoginPageState();
+  }
+  
+  class _LoginPageState extends State<LoginPage> {
+    final TextEditingController emailController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+  
+    @override
+    void initState() {
+      super.initState();
+    }
+  
+    @override
+    Widget build(BuildContext context) {
+      final _formKey = GlobalKey<FormState>();
+      return Scaffold(
+        body: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            
+        ),
+      );
+    }
+  }
