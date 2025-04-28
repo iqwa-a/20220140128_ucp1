8d8906196ec0b6e27eeb1a2903d7ef7ab24636d8
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-04-28 20:43:50 +0700

    membuat piketguda dan membuat controller

---
+++ lib/piket_gudang.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+ 
+ class PiketGudang extends StatefulWidget {
+   const PiketGudang({super.key});
+ 
+   @override
+   State<PiketGudang> createState() => _PiketGudangState();
+ }
+ 
+ class _PiketGudangState extends State<PiketGudang> {
+   final TextEditingController namaController = TextEditingController();
+   final TextEditingController tugasController = TextEditingController();
+ 
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold();
+   }
+ }
