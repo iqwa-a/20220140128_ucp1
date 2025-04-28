127598ee0f0c73e33917b5ede30d2943fd976141
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-04-29 03:49:24 +0700

    add pelanggan

---
+++ lib/pelanggan.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+ 
+ class AddPelanggan extends StatefulWidget {
+   const AddPelanggan({ Key? key }) : super(key: key);
+ 
+   @override
+   _AddPelangganState createState() => _AddPelangganState();
+ }
+ 
+ class _AddPelangganState extends State<AddPelanggan> {
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(
+       appBar: AppBar(
+         title: const Text('Title'),
+       ),
+       body: Container(),
+     );
+   }
+ }
