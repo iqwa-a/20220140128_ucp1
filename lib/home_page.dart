35d5888055ed5efe15d1898ad6d01a1810e97fb7
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-04-28 22:43:11 +0700

    membuat parsing data dan membuat image

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -17,10 +17,11 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       appBar: AppBar(
+        toolbarHeight: 80,
         title: Text('Selamat datang, ${widget.nama}!'),
         centerTitle: true,
         leading: Padding(
-          padding: const EdgeInsets.all(1.0),
+          padding: const EdgeInsets.all(1),
           child: CircleAvatar(
             radius: 50,
             backgroundImage: AssetImage('assets/logo.jpg'),
@@ -30,6 +31,25 @@ class _HomePageState extends State<HomePage> {
           IconButton(onPressed: () {}, icon: const Icon(Icons.add_box)),
         ],
         backgroundColor: const Color.fromARGB(255, 255, 0, 0),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: const DecorationImage(
+                  image: AssetImage('assets/logo2.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+          ],
+        ),
       ),
     );
   }
