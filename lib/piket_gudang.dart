efdeff6e137f1422ec2cd20b9498985f733f0e73
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-04-28 22:28:10 +0700

    tambah pemilihan tanggal menggunakan DatePicker

--- lib/piket_gudang.dart
+++ lib/piket_gudang.dart
@@ -10,6 +10,21 @@ import 'package:flutter/material.dart';
  class _PiketGudangState extends State<PiketGudang> {
    final TextEditingController namaController = TextEditingController();
    final TextEditingController tugasController = TextEditingController();
+    DateTime selectedDate = DateTime.now();
+ 
+   Future<void> _selectedDate(BuildContext context) async {
+     final DateTime? picked = await showDatePicker(
+       context: context,
+       initialDate: selectedDate,
+       firstDate: DateTime(2020),
+       lastDate: DateTime(2100),
+     );
+     if (picked != null && picked != selectedDate) {
+       setState(() {
+         selectedDate = picked;
+       });
+     }
+   }
  
    @override
    Widget build(BuildContext context) {
