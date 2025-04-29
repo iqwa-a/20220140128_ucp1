92450c1dc6e5e76bdefa963644eee99d4cee0835
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-04-29 13:44:06 +0700

    Add DropDown and List

--- lib/barang_page.dart
+++ lib/barang_page.dart
@@ -8,6 +8,22 @@ import 'package:flutter/material.dart';
  }
  
  class _AddBarangState extends State<AddBarang> {
+  late TextEditingController _dateController;
+ 
+   final List<String> jenisTransaksiList = <String>['Barang Masuk', 'Barang Keluar'];
+   String? _selectedJenisTransaksi;
+ 
+   @override
+   void initState() {
+     super.initState();
+     _dateController = TextEditingController();
+   }
+ 
+   @override
+   void dispose() {
+     _dateController.dispose();
+     super.dispose();
+   }
    @override
    Widget build(BuildContext context) {
         final _formkey = GlobalKey<FormState>();
@@ -22,11 +38,73 @@ import 'package:flutter/material.dart';
            child: Container(
              child: Column(
                children: [
-                 const Text('Tanggal Transaksi'),
+                 TextFormField(
+                     controller: _dateController,
+                     keyboardType: TextInputType.none,
+                     decoration: InputDecoration(
+                       labelText: 'Pilih Tanggal',
+                       prefixIcon: new Icon(Icons.calendar_month_rounded),
+                       border: OutlineInputBorder(
+                         borderRadius: BorderRadius.circular(20),
+                       ),
+                     ),
+                     onTap: () async {
+                       DateTime? pickedDate = await showDatePicker(
+                         context: context,
+                         initialDate: DateTime.now(),
+                         firstDate: DateTime(2000),
+                         lastDate: DateTime(2100),
+                       );
+ 
+                       if (pickedDate != null) {
+                         String formattedDate =
+                             "${pickedDate.day}-${pickedDate.month}-${pickedDate.year}";
+                         setState(() {
+                           _dateController.text = formattedDate;
+                         });
+                       }
+                     },
+                     validator: (value) {
+                       if (value == null || value.isEmpty) {
+                         return 'Harap Masukkan Tanggal';
+                       }
+                     },
+                 ),
+                 const SizedBox(
+                   height: 20,
+                 ),
+                 DropdownButtonFormField(
+                   value: _selectedJenisTransaksi,
+                   decoration: InputDecoration(
+                     labelText: 'Pilih Jenis Transaksi',
+                     prefixIcon: const Icon(Icons.filter_1_rounded),
+                     border: OutlineInputBorder(
+                       borderRadius: BorderRadius.circular(20),
+                     ),
+                   ),
+                   items: jenisTransaksiList.map((String value) {
+                     return DropdownMenuItem<String>(
+                       value: value,
+                       child: Text(value),
+                     );
+                   }).toList(),
+                   onChanged: (String? newValue){
+                     setState(() {
+                       _selectedJenisTransaksi = newValue!;
+                     });
+                   },
+                   validator: (value) {
+                     if (value == null || value.isEmpty) {
+                       return 'Harap Pilih Jenis Transaksi';
+                     }
+                     return null;
+                   },
+                 )
                ],
              ),
            ),
          ),
+       ),
      );
    }
  }
