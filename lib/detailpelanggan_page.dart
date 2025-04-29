762023f51dd3a4b57b668f8dd3b31b88cff1924f
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-04-29 10:52:50 +0700

    create detail pelanggan

---
+++ lib/detailpelanggan_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+ 
+ class DetailPelanggan extends StatelessWidget {
+ const DetailPelanggan({ Key? key }) : super(key: key);
+ 
+   @override
+   Widget build(BuildContext context){
+     return Scaffold(
+       appBar: AppBar(
+         title: const Text('Title'),
+       ),
+       body: Container(),
+     );
+   }
+ }
