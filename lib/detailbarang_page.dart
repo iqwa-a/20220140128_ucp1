54e54e733225f7107b7db8e6fdba7e359a931964
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-04-29 14:51:59 +0700

    Full Code layout

--- lib/detailbarang_page.dart
+++ lib/detailbarang_page.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
  
- class DetailBarang extends StatelessWidget {
- const DetailBarang({ Key? key }) : super(key: key);
+ class DetailBarang extends StatelessWidget { 
  
+   final String date;
+   final String jenisTransaksi;
+   final String jenisBarang;
+   final String jumlahBarang;
+   final String hargaSatuan;
+ 
+ const DetailBarang({ Key? key, required this.date, required this.jenisTransaksi, required this.jenisBarang, required this.jumlahBarang, required this.hargaSatuan }) : super(key: key);
    @override
    Widget build(BuildContext context){
      return Scaffold(
@@ -10,7 +17,90 @@ import 'package:flutter/material.dart';
          title: const Text('Title'),
        ),
        body: Form(
-         child: Container(),
+         child: SingleChildScrollView(
+           padding: const EdgeInsets.all(30.0),
+           child: Container(
+             child: Center(
+               child: Column(
+                 children: [
+                   const Text('Detail Barang'),
+                   Image.asset(
+                     'assets/logo3.jpg',
+                     height: 200,
+                     width: 200,
+                   ),
+                   const SizedBox(
+                     height: 20,
+                   ),
+                   const Text('Data Berhasil Disimpan'),
+                   const SizedBox(
+                     height: 20,
+                   ),
+                   const SizedBox(
+                     height: 20,
+                   ),
+                   Row(
+                     children: [
+                       Text('Tanggal'),
+                       const SizedBox(
+                         width: 200,
+                       ),
+                       Text('$date')
+                     ],
+                   ),
+                   const SizedBox(
+                     height: 20,
+                   ),
+                   Row(
+                     children: [
+                       Text('Jenis Transaksi'),
+                       const SizedBox(
+                         width: 140,
+                       ),
+                       Text('$jenisTransaksi')
+                     ],
+                   ),
+                   const SizedBox(
+                     height: 20,
+                   ),
+                   Row(
+                     children: [
+                       Text('Jenis Barang'),
+                       const SizedBox(
+                         width: 160,
+                       ),
+                       Text('$jenisBarang')
+                     ],
+                   ),
+                   const SizedBox(
+                     height: 20,
+                   ),
+                   Row(
+                     children: [
+                       Text('Jumlah Barang'),
+                       const SizedBox(
+                         width: 200,
+                       ),
+                       Text('$jumlahBarang')
+                     ],
+                   ),
+                   const SizedBox(
+                     height: 20,
+                   ),
+                   Row(
+                     children: [
+                       Text('Jenis Harga Satuan'),
+                       const SizedBox(
+                         width: 150,
+                       ),
+                       Text('$hargaSatuan')
+                     ],
+                   ),
+                 ],
+               ),
+             ),
+           ),
+         ),
        ),
      );
    }
